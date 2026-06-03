3b20bb62019bd8b164ed4d3541318a5866076717
CarterLeeAlt <32411143+CarterLeeAlt@users.noreply.github.com>
2026-06-04 06:22:01 +0800

    Hide proxy groups Virtuoso scrollbar

--- src/components/proxy/proxy-groups.tsx
+++ src/components/proxy/proxy-groups.tsx
@@ -1,6 +1,10 @@
-import { useRef } from "react";
+import { forwardRef, useRef } from "react";
 import { useLockFn } from "ahooks";
-import { Virtuoso, type VirtuosoHandle } from "react-virtuoso";
+import {
+  Virtuoso,
+  type ScrollerProps,
+  type VirtuosoHandle,
+} from "react-virtuoso";
 import {
   getConnections,
   providerHealthCheck,
@@ -20,6 +24,29 @@ interface Props {
 
 const VirtuosoHeader = () => <div style={{ height: 4 }} />;
 const VirtuosoFooter = () => <div style={{ height: 4 }} />;
+
+type ProxyGroupsScrollerProps = ScrollerProps & { className?: string };
+
+const ProxyGroupsScroller = forwardRef<
+  HTMLDivElement,
+  ProxyGroupsScrollerProps
+>(({ className, ...props }, ref) => (
+  <div
+    {...props}
+    ref={ref}
+    className={[className, "proxy-groups-virtuoso-scroller"]
+      .filter(Boolean)
+      .join(" ")}
+  />
+));
+
+ProxyGroupsScroller.displayName = "ProxyGroupsScroller";
+
+const virtuosoComponents = {
+  Header: VirtuosoHeader,
+  Footer: VirtuosoFooter,
+  Scroller: ProxyGroupsScroller,
+};
 
 export const ProxyGroups = (props: Props) => {
   const { mode } = props;
@@ -123,11 +150,12 @@ export const ProxyGroups = (props: Props) => {
   return (
     <Virtuoso
       ref={virtuosoRef}
+      className="proxy-groups-virtuoso"
       style={{ height: "100%" }}
       totalCount={renderList.length}
       defaultItemHeight={64}
       increaseViewportBy={256}
-      components={{ Header: VirtuosoHeader, Footer: VirtuosoFooter }}
+      components={virtuosoComponents}
       itemContent={(index) => (
         <ProxyRender
           key={renderList[index].key}
