9c07f04aec2714fe965103027d50bf9850d19abd
CarterLeeAlt <32411143+CarterLeeAlt@users.noreply.github.com>
2026-06-04 05:24:34 +0800

    Update proxy-groups.tsx

--- src/components/proxy/proxy-groups.tsx
+++ src/components/proxy/proxy-groups.tsx
@@ -18,8 +18,8 @@ interface Props {
   mode: string;
 }
 
-const VirtuosoHeader = () => <div style={{ height: 8 }} />;
-const VirtuosoFooter = () => <div style={{ height: 16 }} />;
+const VirtuosoHeader = () => <div style={{ height: 5 }} />;
+const VirtuosoFooter = () => <div style={{ height: 5 }} />;
 
 export const ProxyGroups = (props: Props) => {
   const { mode } = props;
