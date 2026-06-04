0de7404d01f7c2b5c5b361154c168383c85ab669
CaryrtLeeWin <carterlee@nmail.edu.kg>
2026-06-04 21:25:46 +0800

    sync non-github changes from alpha

--- src/components/base/base-notice.tsx
+++ src/components/base/base-notice.tsx
@@ -32,7 +32,7 @@ export const NoticeManager = () => {
     <Box
       sx={{
         position: "fixed",
-        top: "88px",
+        top: "60px",
         right: "20px",
         zIndex: 1500,
         display: "flex",
@@ -52,6 +52,13 @@ export const NoticeManager = () => {
             width: "100%",
             pointerEvents: "auto",
             wordBreak: "break-word",
+            color: "#fff",
+            "& .MuiAlert-icon": {
+              color: "#fff",
+            },
+            "& .MuiAlert-action": {
+              color: "#fff",
+            },
           }}
           action={
             <IconButton
